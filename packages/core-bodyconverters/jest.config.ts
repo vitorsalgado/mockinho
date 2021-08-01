e75a261aa85d4b61db1c5eda21e013a720c23c95
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-08-01 00:17:45 -0400

    refactor: add ts-ignore to local jest configs

--- packages/core-bodyconverters/jest.config.ts
+++ packages/core-bodyconverters/jest.config.ts
@@ -1,4 +1,6 @@
 import type { Config as JestConfig } from '@jest/types'
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import Base from '../../jest-base.config'
 
 const config: JestConfig.InitialOptions = {
