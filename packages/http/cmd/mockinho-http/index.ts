dd5e3e9b0539b8dc50851a05af822df36098f398
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-07-31 22:13:45 -0400

    fix: build

--- packages/http/cmd/mockinho-http/index.ts
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env node
-
-import { mockinhoHTTP, opts } from '../../pkg'
-
-mockinhoHTTP(opts().port(3000).dynamicPort(false).loadFileStubs().verbose())
-  .start()
-  .then()
-  .catch(console.error)
