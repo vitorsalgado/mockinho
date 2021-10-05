7d1fdaa35d92f83fdb4b03266edeee41ae644742
Vitor Hugo Salgado <vsalgadopb@gmail.com>
2021-10-04 23:17:48 -0300

    refactor: rename http-jest to http-jest-extension

--- http-jest-extension/jest.config.ts
+++ http-jest-extension/jest.config.ts
@@ -4,7 +4,7 @@ import type { Config as JestConfig } from '@jest/types'
 import Base from '../jest-base.config'
 
 const config: JestConfig.InitialOptions = {
-  displayName: 'http-jest-environment',
+  displayName: 'http-jest-extension',
   globals: {
     'ts-jest': {
       tsconfig: '../tsconfig.test.json'
